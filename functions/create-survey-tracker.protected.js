d17de5e3a6a569b49d5904d4e683a4bfa017928a
Jim Madden <jmadden@twilio.com>
2021-12-09 09:15:27 -0800

    added SMRedirectory

--- functions/create-survey-tracker.protected.js
+++ functions/create-survey-tracker.protected.js
@@ -1,22 +1,18 @@
 exports.handler = async (context, event, callback) => {
-  console.log('CREATE IVR - CALL SID: ', event.callSid, event.from);
-
-  let from_number = event.from;
-
-  let client = context.getTwilioClient();
-
-  let conversations = {};
-  conversations.conversation_id = event.callSid;
-
+  const client = context.getTwilioClient();
+  const attributes = {
+    callSurvey: event.callSid,
+    conversations: { conversation_id: event.callSid },
+  };
   try {
     const response = await client.taskrouter
       .workspaces(context.TWILIO_WORKSPACE_SID)
       .tasks.create({
-        attributes: JSON.stringify({ from: from_number, conversations }),
+        attributes: JSON.stringify({ callSurvey: conversations }),
         workflowSid: context.TWILIO_NOBODY_WORKFLOW_SID,
         timeout: 300,
       });
-    return callback(null, response.sid);
+    return callback();
   } catch (error) {
     return callback(`Error -- Survey Task Not Created: ${error}`);
   }
--- functions/update-survey-tracker.protected.js
+++ functions/update-survey-tracker.protected.js
@@ -1,3 +1,50 @@
-exports.handler = function (context, event, callback) {
-  callback(null, null);
+exports.handler = async (context, event, callback) => {
+  let digits = event.digits;
+
+  let client = context.getTwilioClient();
+
+  let taskFilter = `callSurvey == '${event.callSid}'`;
+
+  //search for the task based on the CallSid attribute
+  const tasks = client.taskrouter
+    .workspaces(context.TWILIO_WORKSPACE_SID)
+    .tasks.list({ evaluateTaskAttributes: taskFilter })
+    .then((tasks) => {
+      console.log(tasks);
+      let taskSid = tasks[0].sid;
+      let attributes = { ...JSON.parse(tasks[0].attributes) };
+      let IVR_time = Math.round(
+        (IVR_end - attributes.conversations.IVR_time_start) / 1000
+      );
+
+      //was the call abandoned?
+      if (callStatus == 'completed') {
+        attributes.conversations.abandoned = 'Yes';
+        attributes.conversations.abandoned_phase = 'Survey';
+      } else {
+        attributes.conversations.abandoned = 'No';
+        attributes.conversations.abandoned_phase = null;
+      }
+
+      //update the task
+      client.taskrouter
+        .workspaces(context.TWILIO_WORKSPACE_SID)
+        .tasks(taskSid)
+        .update({
+          assignmentStatus: 'canceled',
+          reason: 'IVR task',
+          attributes: JSON.stringify(attributes),
+        })
+        .then((task) => {
+          callback();
+        })
+        .catch((error) => {
+          console.log(error);
+          callback(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      callback(error);
+    });
 };
