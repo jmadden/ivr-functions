0980c2954128c23a16a121f83738038ff8fbfa58
Jim Madden <jmadden@twilio.com>
2021-12-09 08:05:06 -0800

    get number logic changes

--- functions/create-survey-tracker.protected.js
+++ functions/create-survey-tracker.protected.js
@@ -1,3 +1,23 @@
-exports.handler = function (context, event, callback) {
-  callback(null, null);
+exports.handler = async (context, event, callback) => {
+  console.log('CREATE IVR - CALL SID: ', event.callSid, event.from);
+
+  let from_number = event.from;
+
+  let client = context.getTwilioClient();
+
+  let conversations = {};
+  conversations.conversation_id = event.callSid;
+
+  try {
+    const response = await client.taskrouter
+      .workspaces(context.TWILIO_WORKSPACE_SID)
+      .tasks.create({
+        attributes: JSON.stringify({ from: from_number, conversations }),
+        workflowSid: context.TWILIO_NOBODY_WORKFLOW_SID,
+        timeout: 300,
+      });
+    return callback(null, response.sid);
+  } catch (error) {
+    return callback(`Error -- Survey Task Not Created: ${error}`);
+  }
 };
